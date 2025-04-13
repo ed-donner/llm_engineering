3f7816d684448eb4184aa01f84b599bd14f37e2f
craigprobus <craigprobus@gmail.com>
2025-04-13 15:18:08 -0700

    adding REVISION variable back to file

--- week8/pricer_service2.py
+++ week8/pricer_service2.py
@@ -16,6 +16,7 @@ PROJECT_NAME = "pricer"
 HF_USER = "ed-donner" # your HF name here! Or use mine if you just want to reproduce my results.
 RUN_NAME = "2024-09-13_13.04.39"
 PROJECT_RUN_NAME = f"{PROJECT_NAME}-{RUN_NAME}"
+REVISION = "e8d637df551603dc86cd7a1598a8f44af4d7ae36"
 FINETUNED_MODEL = f"{HF_USER}/{PROJECT_RUN_NAME}"
 CACHE_DIR = "/cache" 
 
