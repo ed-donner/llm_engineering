87ac4e548fb172ffaaf7dcff50664775a2f6ea0e
Maksym Litvinenko <maksym.litvinenko@zfort.com>
2026-02-27 20:42:13 +0100

    update the sample with local open source model

--- week1/community-contributions/massonus/crypto_tutor.py
+++ week1/community-contributions/massonus/crypto_tutor.py
@@ -1,20 +1,15 @@
-import os
-
-from dotenv import load_dotenv
 from openai import OpenAI
 
 from week1.scraper import fetch_website_contents
 
-load_dotenv(override=True)
-api_key = os.getenv("OPENAI_API_KEY")
+deep_seek = OpenAI(base_url="http://127.0.0.1:11434/v1", api_key="deepseek")
 
-openai = OpenAI(api_key=api_key)
 website_contents = fetch_website_contents(
     "https://www.coinbase.com/learn/crypto-basics"
 )
 
 system_prompt = "You are a helpful crypto tutor. You are an expert in crypto and blockchain. You are able to answer questions about crypto and blockchain."
-user_prompt = """
+user_prompt = f"""
     Make a short summary of the following website:
     {website_contents}
 """
@@ -27,7 +22,9 @@ messages = [
 ]
 
 # Step 3: Call OpenAI
-response = openai.chat.completions.create(model="gpt-4.1-nano", messages=messages)
+response = deep_seek.chat.completions.create(
+    model="deepseek-r1:1.5b", messages=messages
+)
 
 # Step 4: print the result
 print(response.choices[0].message.content)
