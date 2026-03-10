39f12dea57f4effc55b785d17891ed540e30ab9a
damien savoldelli <damien.savoldelli@gmail.com>
2026-03-10 19:08:01 +0100

    Simplify Week 1 notebooks: reduced verbosity, removed extra markdown, streamlined implementations

--- community-contributions/damien_sa/week1/scraper.py
+++ community-contributions/damien_sa/week1/scraper.py
@@ -1,17 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-
 # Standard headers to fetch a website
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 }
 
-
 def fetch_website_contents(url):
     """
-    Return the title and contents of the website at the given url;
-    truncate to 2,000 characters as a sensible limit
+    Return the title and contents of the website at the given url; truncate to 2,000 characters as a sensible limit
     """
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -24,12 +21,9 @@ def fetch_website_contents(url):
         text = ""
     return (title + "\n\n" + text)[:2_000]
 
-
 def fetch_website_links(url):
     """
     Return the links on the webiste at the given url
-    I realize this is inefficient as we're parsing twice! This is to keep the code in the lab simple.
-    Feel free to use a class and optimize it!
     """
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
