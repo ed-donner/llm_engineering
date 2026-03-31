29f3434e4b3a9e567df05d1743d5e850f9a1311b
Kaustuv Mukherjee <km@Kaustuvs-MacBook-Pro.local>
2026-03-31 12:44:15 +1100

    Kaustuv - Community contribution- AI Tutor Chrome plugin

--- week1/community-contributions/kaustuv/main.py
+++ week1/community-contributions/kaustuv/main.py
@@ -11,16 +11,34 @@ MODEL_LLAMA = 'llama3.2'
 LLAMA_URL = "http://localhost:11434/v1"
 
 MODEL_GPT = "gpt-5.4-mini"
+
 load_dotenv(override=True)
 OPENAI_KEY = os.getenv("OPEN_API_KEY")
 
-client = OpenAI()
+def get_LLM(local= False):
+    client = None
+    if local:
+        client = OpenAI(base_url=LLAMA_URL,api_key="ollama")
+        print(f'Serving local model')
+    else:
+        client = OpenAI()
+        print(f'Serving PAID model')
+    return client
 
-response = client.chat.completions.create(model=MODEL_GPT,
+def getModel(local = False):
+    if local:
+        return MODEL_LLAMA
+    else:
+        return MODEL_GPT
+
+client = get_LLM()
+mod = getModel()
+response = client.chat.completions.create(model= mod,
     messages=[{"role":"user","content":"Tell me a joke"}]
     )
     
 print(response.choices[0].message.content)
+
 # 1. Initialize the app first
 app = FastAPI()
 
@@ -50,20 +68,11 @@ class WritingDraft(BaseModel):
 async def analyze_writing(draft: WritingDraft):
     try:
         print(f"--- Processing New Draft ({len(draft.text)} chars) ---")
-        
-        '''response = client.chat.completions.create(
-            model="gpt-4o-mini", 
-            messages=[
-                {"role": "system", "content": "You are a tutor. Output JSON with keys: 'Overall score out of 25', 'Detailed Feedback': {'Good': '...', 'Improve': '...'}"},
-                {"role": "user", "content": draft.text}
-            ],
-            response_format={"type": "json_object"}
-        )'''
-        
+                
         system_prompt2 = """You are an expert tutor in Non-Narrative writing for Selective Tests in Australia. 
         Assess the writing based STRICTLY on the provided rubrics for Content, Form, Organisation, Style, and Expression.
         Provide a score out of 25.
-        Output MUST be a valid JSON object only. No markdown, no code blocks, no preamble.
+        Output MUST be a valid JSON object only. No markdown, no code blocks, no preamble. For Detailed Analysis results You MUST focus only on the most important 5 improvements which will maximise the score.
 
         RUBRICS:
         [Content, Form, Organisation & Style]
@@ -97,10 +106,18 @@ async def analyze_writing(draft: WritingDraft):
             "Good": "string (encouraging and enthusiastic)",
             "Improve": "string (concise and precise with examples)"
         }
-        }"""
+        }
 
+        VERY VERY IMPORTANT FOR DETAILS
+        1. DO ensure the tone is ECOURAGING at all times
+        2. DO ensure examples are clear
+        3. DO NOT include invalid line numbers
+        4. DO NOT include more than 5 most important changes in the Details array. 
+          
+        """
+        
         response = client.chat.completions.create(
-            model=MODEL_GPT, 
+            model=mod, #MODEL_GPT, 
             messages=[
                 {"role": "system", "content": system_prompt2},
                 {"role": "user", "content": draft.text}
@@ -129,3 +146,5 @@ if __name__ == "__main__":
     import uvicorn
     # Do NOT use reload=True if running via this block to avoid middleware issues
     uvicorn.run(app, host="127.0.0.1", port=8000)
+   # uvicorn.run(app, host="0.0.0.0", port=8080)
+
